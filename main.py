91095692aac99a35cae24e0d7da3359f886412cf
yuripiffer <yuripiffer@hotmail.com>
2021-10-17 17:21:00 -0300

    Ana ate chover canivete

--- main.py
+++ main.py
@@ -7,10 +7,11 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print("Maria")
+    print("Sou a Ana sim minha querida")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Ana ate o fim")
