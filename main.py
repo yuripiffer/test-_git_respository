9a8ff19f0fe08b27a0ff6b499a6818bdafc69a62
yuripiffer <yuripiffer@hotmail.com>
2021-10-17 17:18:11 -0300

    New Function

--- main.py
+++ main.py
@@ -7,10 +7,11 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print("Maria")
+    print("Maria do Bairro Bebe")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print("New Function")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
