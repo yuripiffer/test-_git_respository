77264bc0d934657413c3f5482498e2a2417f4c9c
yuripiffer <yuripiffer@hotmail.com>
2021-10-17 17:28:02 -0300

    Testing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.ASDLKJFHASDLFKJH
 
 
 def print_hi(name):
@@ -14,4 +14,4 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     print("New Function")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/BLABLABLA
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
